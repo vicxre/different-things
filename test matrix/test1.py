819fc106aa98d0de98414db7b2ddba9ce9fb4310
kir <kirskor1337@gmail.com>
2025-07-02 22:06:33 +0300

    u got me

--- test matrix/test1.py
+++ test matrix/test1.py
@@ -10,9 +10,7 @@ shirina = int(input('ввеидте ширину матрицы ->'))
 matr1 = [[r.randint(1,10) for _ in range(shirina)] for _ in range(visota)]
 print('матрица №1 do', matr1)
 
-
-matr2 = [[r.randint(1,10) for _ in range(3)] for _ in range(3)]
-print('матрица №2 do', matr2)
+x = print(eval(input('-> ')))
 
 
 
